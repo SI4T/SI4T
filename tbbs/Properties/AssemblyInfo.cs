c780dbdc0432c56e7a9077c2366ee76996890607
Will Price <willpricework@gmail.com>
2014-04-17 11:13:03 +0200

    Updated AssemblyInfo.cs

--- tbbs/Properties/AssemblyInfo.cs
+++ tbbs/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Viking.Cruises.Oceans.Templating")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SI4T.Templating")]
+[assembly: AssemblyDescription("SDL Tridion Template Building Blocks to prepare content for indexing by a search engine")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard")]
-[assembly: AssemblyProduct("Viking.Cruises.Oceans.Templating")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2013")]
+[assembly: AssemblyCompany("SI4T")]
+[assembly: AssemblyProduct("SI4T Framework")]
+[assembly: AssemblyCopyright("Copyright © Will Price 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
