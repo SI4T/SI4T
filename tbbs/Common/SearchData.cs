344f244c4081112a1dd38dc5e2a658532b570ef8
nnarvaria <nnarvaria@sdl.com>
2019-02-18 14:46:07 +0530

    Added fix of SI4T with Sites 9, get indexed Data from Regions and ComponentPresentation both

--- tbbs/Common/SearchData.cs
+++ tbbs/Common/SearchData.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
 using Tridion.ContentManager.CommunicationManagement;
+using Tridion.ContentManager.CommunicationManagement.Regions;
 using Tridion.ContentManager.ContentManagement;
 using Tridion.ContentManager.ContentManagement.Fields;
 
@@ -14,43 +15,35 @@ namespace SI4T.Templating
     /// be subclassed and extended/altered with ease
     /// </summary>
     [XmlRoot("indexdata")]
-    public class SearchData 
+    public class SearchData
     {
         //Url for the item, not this can also be a uri or other identifier used to load a resource
-        [XmlElement("url")]
-        public string Url { get; set; }
-        
+        [XmlElement("url")] public string Url { get; set; }
+
         //Title to show when results displayed
-        [XmlElement("title")]
-        public string Title { get; set; }
-        
-        [XmlElement("publicationid")]
-        public int PublicationId { get; set; }
-        
-        [XmlElement("schemaid")]
-        public int SchemaId { get; set; }
+        [XmlElement("title")] public string Title { get; set; }
+
+        [XmlElement("publicationid")] public int PublicationId { get; set; }
+
+        [XmlElement("schemaid")] public int SchemaId { get; set; }
 
         //16 for component, 64 for page
-        [XmlElement("itemtype")]
-        public int ItemType { get; set; }
-        
-        [XmlElement("parentsgid")]
-        public int ParentSGId { get; set; }
-        
+        [XmlElement("itemtype")] public int ItemType { get; set; }
+
+        [XmlElement("parentsgid")] public int ParentSGId { get; set; }
+
         //All the parent, grandparent etc. SGs
-        [XmlElement("sgid")]
-        public List<int> StructureGroups { get; set; }
-        
+        [XmlElement("sgid")] public List<int> StructureGroups { get; set; }
+
         //Used to enable searches to be restricted to certain areas, and to exclude content from general search
-        [XmlElement("type")]
-        public int Type { get; set; }
+        [XmlElement("type")] public int Type { get; set; }
 
         //Used to process content and metadata field sets and build up index data
         private FieldProcessor _processor;
-        
+
         //flag indicating that the item processed is to be indexed
         private bool _hasIndexData = false;
-        
+
         public SearchData(FieldProcessor processor)
             : this()
         {
@@ -73,19 +66,20 @@ namespace SI4T.Templating
             if (page.IsIndexed() && page.PageTemplate.IsIndexed() && page.OrganizationalItem.IsIndexed())
             {
                 this.Url = page.PublishLocationUrl;
-                this.SchemaId = page.ComponentPresentations.Count > 0 ? page.ComponentPresentations[0].Component.Schema.Id.ItemId: 0;
+                this.SchemaId = FetchSchemaId(page);
                 this.PublicationId = page.ContextRepository.Id.ItemId;
                 this.ItemType = 64;
                 this.ParentSGId = page.OrganizationalItem.Id.ItemId;
                 StructureGroups = new List<int>();
                 var sg = page.OrganizationalItem;
-                while (sg!=null && sg is StructureGroup)
+                while (sg != null && sg is StructureGroup)
                 {
                     StructureGroups.Add(sg.Id.ItemId);
                     sg = sg.OrganizationalItem;
                 }
                 ProcessPageMetadata(page);
-                foreach (var cp in page.ComponentPresentations)
+                List<ComponentPresentation> cps = GatherAllCpsFromPage(page);
+                foreach (var cp in cps)
                 {
                     if (cp.ComponentTemplate.IsIndexed(_processor.MinimumComponentTemplatePrio))
                     {
@@ -101,6 +95,7 @@ namespace SI4T.Templating
                         }
                     }
                 }
+
                 if (String.IsNullOrEmpty(_processor.Title))
                 {
                     this.Title = page.Title;
@@ -110,8 +105,81 @@ namespace SI4T.Templating
                     this.Title = _processor.Title;
                 }
             }
+
             return processedDcps;
         }
+
+        /// <summary>
+        /// Fetch Schema Id from Page eithor from first CP or first Region CP
+        /// </summary>
+        /// <param name="page"></param>
+        /// <returns>SchemaId</returns>
+        private int FetchSchemaId(Page page)
+        {
+            if (page.ComponentPresentations != null && page.ComponentPresentations.Count > 0)
+            {
+                return page.ComponentPresentations[0].Component.Schema.Id.ItemId;
+            }
+            else
+            {
+                if (page.Regions != null && page.Regions.Count > 0)
+                {
+                    var regionCps = page.Regions[0].ComponentPresentations;
+                    if (regionCps != null && regionCps.Count > 0)
+                    {
+                        return regionCps[0].Component.Schema.Id.ItemId;
+                    }
+                }
+            }
+            return 0;
+
+        }
+
+        /// <summary>
+        /// Gather All Component Presentations from Page or Page Regions
+        /// </summary>
+        /// <param name="page"></param>
+        /// <returns>List ComponentPresentation</returns>
+        private List<ComponentPresentation> GatherAllCpsFromPage(Page page)
+        {
+            List<ComponentPresentation> cps = new List<ComponentPresentation>();
+            if (page.ComponentPresentations != null && page.ComponentPresentations.Count > 0)
+            {
+                cps.AddRange(page.ComponentPresentations);
+            }
+
+            if (page.Regions != null && page.Regions.Count > 0)
+            {
+                cps.AddRange(GetCpsFromRegions(page.Regions));
+            }
+
+            return cps;
+        }
+
+        /// <summary>
+        /// Get Component Presentations from Regions Recursivly
+        /// </summary>
+        /// <param name="pageRegions"></param>
+        /// <returns>List ComponentPresentation</returns>
+        private List<ComponentPresentation> GetCpsFromRegions(IList<IRegion> pageRegions)
+        {
+            List<ComponentPresentation> cps = new List<ComponentPresentation>();
+            foreach (var pageRegion in pageRegions)
+            {
+                if (pageRegion.ComponentPresentations != null && pageRegion.ComponentPresentations.Count > 0)
+                {
+                    cps.AddRange(pageRegion.ComponentPresentations);
+                }
+
+                if (pageRegion.Regions != null && pageRegion.Regions.Count > 0)
+                {
+                    cps.AddRange(GetCpsFromRegions(pageRegion.Regions));
+                }
+            }
+
+            return cps;
+        }
+
 
         /// <summary>
         /// Prepare index data for a component presentation
@@ -121,7 +189,8 @@ namespace SI4T.Templating
         public virtual void ProcessComponentPresentation(ComponentPresentation cp, List<string> flaggedDcps)
         {
             string id = GetDcpIdentifier(cp);
-            if (cp.ComponentTemplate.IsIndexed(_processor.MinimumComponentTemplatePrio) && (flaggedDcps == null || !flaggedDcps.Contains(id)))
+            if (cp.ComponentTemplate.IsIndexed(_processor.MinimumComponentTemplatePrio) &&
+                (flaggedDcps == null || !flaggedDcps.Contains(id)))
             {
                 this.Url = GetUrlForDcp(cp);
                 FieldProcessorSettings settings = cp.ComponentTemplate.GetFieldProcessorSettings();
@@ -193,6 +262,7 @@ namespace SI4T.Templating
                     ItemFields fields = new ItemFields(component.Content, component.Schema);
                     _processor.ProcessData(fields, settings);
                 }
+
                 ProcessMetadata(component, settings);
             }
         }
@@ -251,7 +321,8 @@ namespace SI4T.Templating
         /// <returns>Id in the form: dcp:{pubid}-{compid}-{ctid}</returns>
         public virtual string GetDcpIdentifier(ComponentPresentation cp)
         {
-            return GetDcpIdentifier(cp.Component.Id.PublicationId, cp.Component.Id.ItemId, cp.ComponentTemplate.Id.ItemId);
+            return GetDcpIdentifier(cp.Component.Id.PublicationId, cp.Component.Id.ItemId,
+                cp.ComponentTemplate.Id.ItemId);
         }
 
         /// <summary>
